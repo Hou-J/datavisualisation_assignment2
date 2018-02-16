7b767298bf467d26a00890c53f9b34bf38d8ba51
Jiongxu Hou <jiongxuhou@gmail.com>
2018-02-16 21:57:45 +0000

    add temperature, next step, add date

--- d3_task2.js
+++ d3_task2.js
@@ -5,9 +5,9 @@ var canvas = d3.select("body")
 
 d3.csv("333.csv", function (data3) {
     function color(n, l) {
-        if (n === "1") {return l === "A" ? "#2a50a0" : "#00cfef";}
-        else if (n === "2") {return l === "A" ? "#cb3512" : "#ffb000"}
-        else if (n === "3") {return l === "A" ? "#109618" : "#02ec6a"}
+        if (n === "1") {return l === "A" ? "#3b7adf" : "#00cfef";}
+        else if (n === "2") {return l === "A" ? "#be611f" : "#ffb312"}
+        else if (n === "3") {return l === "A" ? "#109618" : "#10ec00"}
     }
 
     var lines = canvas
@@ -26,7 +26,7 @@ d3.csv("333.csv", function (data3) {
 });
 
 d3.csv("111.csv", function (data1) {
-    canvas.append("g")
+    var circles = canvas.append("g")
         .selectAll("circle")
         .data(data1)
         .enter()
@@ -35,7 +35,7 @@ d3.csv("111.csv", function (data1) {
             .attr("cy", function (d) {return (60 - d.LATC) * 100 - 400;})
             .attr("r", 5);
 
-    canvas.append("g")
+    var texts = canvas.append("g")
         .selectAll("text")
         .data(data1)
         .enter()
@@ -46,6 +46,55 @@ d3.csv("111.csv", function (data1) {
             .text(function (d) {return d.CITY;});
 });
 
-d3.csv("222.csv", function (data2) {
+var scale = d3.scaleLinear()
+    .domain([-30, 0])
+    .range([300, 0]);
 
+var y_axis = d3.axisRight()
+    .ticks(6)
+    .scale(scale);
+
+canvas.append("g")
+    .attr("transform", "translate(1470,300)")
+    .call(y_axis);
+
+line_data = [
+    {p: [{x: 0, y: 0}, {x: 1365, y: 0}]},
+    {p: [{x: 0, y: 50}, {x: 1365, y: 50}]},
+    {p: [{x: 0, y: 100}, {x: 1365, y: 100}]},
+    {p: [{x: 0, y: 150}, {x: 1365, y: 150}]},
+    {p: [{x: 0, y: 200}, {x: 1365, y: 200}]},
+    {p: [{x: 0, y: 250}, {x: 1365, y: 250}]},
+    {p: [{x: 0, y: 300}, {x: 1365, y: 300}]}
+];
+
+var line = d3.line()
+    .x(function(d) { return d.x; })
+    .y(function(d) { return d.y; });
+
+canvas.append("g")
+    .attr("transform", "translate(105,300)")
+    .selectAll('path')
+    .data(line_data)
+    .enter()
+        .append('path')
+        .attr('d', function(d) { return line(d.p); })
+        .attr('stroke-width', 0.5)
+        .attr('stroke', "#a9a9a9");
+
+d3.csv("222.csv", function (data2) {
+    var line = d3.line()
+        .x(function (d) { return (d.LONT-23) * 100})
+        .y(function (d) { return -d.TEMP * 10});
+
+    canvas.append("g")
+        .attr("transform","translate(0, 300)")
+        .selectAll("path")
+        .data([data2])
+        .enter()
+            .append("path")
+            .attr("d", line)
+            .attr("fill", "none")
+            .attr("stroke", "black")
+            .attr("stroke-width", 2);
 });
